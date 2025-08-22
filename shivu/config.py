158f2ddb0bf689a2f6eae27fea8d88e371f9d8c4
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-08-23 01:51:51 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,14 +5,13 @@ class Config(object):
     OWNER_ID = "6596417062"
     sudo_users = "7576729648", "6596417062"
     GROUP_ID = -1002955024896
-    CHARA_CHANNEL_ID = -1002527530412   # yaha apne private channel ka ID dalna
     TOKEN = "8288304784:AAH-UOflloHLYkwU7ji32Q9tx_7o1JYeQLQ"
     mongo_url = "mongodb+srv://sufyan532011:5042@auctionbot.5ms20.mongodb.net/?retryWrites=true&w=majority&appName=AuctionBot"
     PHOTO_URL = ["https://files.catbox.moe/n4sucx.jpg", "https://files.catbox.moe/n4sucx.jpg"]
     SUPPORT_CHAT = "strawberry_graveyard"
     UPDATE_CHAT = "Naruto_X_Waifu"
     BOT_USERNAME = "Nguess_Zangetsu_bot"
-    CHARA_CHANNEL_ID = "-1002133191051"
+    CHARA_CHANNEL_ID = "-1002527530412"
     api_id = 21218274
     api_hash = "3474a18b61897c672d315fb330edb213"
 
